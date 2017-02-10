8c82c251271734c89bfd9a544947f42ac4378004
Noel Parisi <NoelParisi@Noels-MacBook-Pro.local>
2017-02-10 15:37:00 -0800

    more javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,36 @@
 var options = ["nickelodeon", "baywatch", "friends","goosebumps", "fullhouse", "rugrats"];
 var wins = 0;
-var currentWordArr;
 var alreadyguessed = [];
 var correctlyGuessed = [];
 var guessesremaining = 13;
 var computerChoose = function() {
 		return options[Math.floor(Math.random() * options.length)];
-	}
+	};
 var currentWord = computerChoose();
-var currentWordArr = currentWord.split("")
+var currentWordArr = currentWord.split("");
+correctlyGuessed.length = currentWordArr.length;
+console.log(correctlyGuessed)
+console.log(currentWordArr);
 
 	document.onkeyup = function(event) {
-
 		var userGuess = event.key;
+		var letter = userGuess;
+		console.log(letter);
 		console.log("You chose "+ userGuess); 
-		alreadyguessed.push(userGuess);
-		console.log(alreadyguessed);
 
-		console.log(currentWordArr);
-
-		for (i=0; i < currentWordArr.length; i ++) {
-			if (userGuess === currentWordArr[i]) {
-				correctlyGuessed.push(userGuess);
+		//trying to find out a way to get duplicated letters indices
+		//should the 1 in my splice be 0?
+  		for(i = 0; i < currentWordArr.length; i++){
+  			if (currentWordArr[i] === letter){
+  				correctlyGuessed.splice(currentWordArr.indexOf(letter), 1, letter);
 				console.log(correctlyGuessed);
-			}
-		}
-	}
+  			}
+
+  		}
+
+  		alreadyguessed.push(letter);
+
+  	}
 //create an array to contain the guessed letter
 //create an array to contain the letters guessed that're in the winning word
 //split the word choosen by the computer into an array 
