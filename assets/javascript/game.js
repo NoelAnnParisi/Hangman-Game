b6f5318d770edddbd4abdd65159c19a9bb669175
Noel Parisi <NoelParisi@calvisitor-10-105-176-54.calvisitor.1918.berkeley.edu>
2017-02-11 15:53:40 -0800

    fixes arrays and html display properties

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// try and make this into an object 
+
 var options = ["nickelodeon", "baywatch", "friends","goosebumps", "fullhouse", "rugrats"];
 var wins = 0;
 var alreadyguessed = [];
@@ -9,40 +11,45 @@ var computerChoose = function() {
 var currentWord = computerChoose();
 var currentWordArr = currentWord.split("");
 correctlyGuessed.length = currentWordArr.length;
-console.log(correctlyGuessed)
+
+var htmlReplacer = function htmlReplacer (id, html) {
+	document.querySelector(id).innerHTML = html;
+}
+
 console.log(currentWordArr);
 
-	document.onkeyup = function(event) {
-		var userGuess = event.key;
-		var letter = userGuess;
-		console.log(letter);
-		console.log("You chose "+ userGuess); 
 
-		//trying to find out a way to get duplicated letters indices
-		//should the 1 in my splice be 0
-	  		for(i = 0; i < currentWordArr.length;i++){
+
+	document.onkeyup = function(event) {
+		var letter = event.key;
+		console.log(letter);
+		console.log("You chose "+ letter); 
+
+		var guessedLetters = function guessedLetters(letter, alreadyguessed) {
+			if (alreadyguessed.length === 0) {
+				alreadyguessed.push(letter);
+				htmlReplacer("#alreadyguessed", alreadyguessed);
+			}
+		};
+			
+		var guessedLettersrepeat = function guessedLettersrepeat (letter, alreadyguessed) {
+			if (alreadyguessed.length >= 1 && !alreadyguessed.includes(letter)) {
+				alreadyguessed.push(letter);
+				htmlReplacer("#alreadyguessed", alreadyguessed);
+			}
+		};
+			
+
+	  	var displayWord = function displayWord(currentWordArr, correctlyGuessed) {
+	  		for(let i = 0; i < currentWordArr.length;i++){
 	  			if (letter === currentWordArr[i]){
 	  				correctlyGuessed.splice(currentWordArr.indexOf(letter, [i]), 1, letter);
-					console.log(correctlyGuessed);
+					htmlReplacer("#correctlyGuessed", correctlyGuessed);
 	  			}
 	  		}
+	  	};
 
-
-	  	//how will I get a repeated letter not to count? 
-  		if userGuess === letter {
-  			alreadyguessed.push(letter);
-	  		console.log(alreadyguessed);
-  		}
-
-  		//Think about how to tally up the wins and already guessed letters
-  		//How to get all of this to display on my html page 
-	  		
-
-  	}
-//create an array to contain the guessed letter
-//create an array to contain the letters guessed that're in the winning word
-//split the word choosen by the computer into an array 
-//compare the array with the user input
-//decrease the number of guesses remaining by one each time a letter in typed 
-	// if the letter typed has already been used do not decrease the # of guesses 
-// 
+	  	guessedLetters(letter, alreadyguessed);
+	  	guessedLettersrepeat(letter, alreadyguessed);
+	  	displayWord(currentWordArr, correctlyGuessed); 
+	}
