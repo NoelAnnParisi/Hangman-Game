fabcd6b566df6bf5b4c88b28497307ded23aef23
Noel Parisi <NoelParisi@calvisitor-10-105-176-54.calvisitor.1918.berkeley.edu>
2017-02-11 16:11:52 -0800

    fixes js loops and simplifies functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-// try and make this into an object 
-
 var options = ["nickelodeon", "baywatch", "friends","goosebumps", "fullhouse", "rugrats"];
 var wins = 0;
 var alreadyguessed = [];
@@ -18,38 +16,37 @@ var htmlReplacer = function htmlReplacer (id, html) {
 
 console.log(currentWordArr);
 
-
-
 	document.onkeyup = function(event) {
 		var letter = event.key;
 		console.log(letter);
 		console.log("You chose "+ letter); 
 
 		var guessedLetters = function guessedLetters(letter, alreadyguessed) {
-			if (alreadyguessed.length === 0) {
+			if (alreadyguessed.length === 0 && !currentWordArr.includes(letter)) {
 				alreadyguessed.push(letter);
 				htmlReplacer("#alreadyguessed", alreadyguessed);
 			}
 		};
 			
 		var guessedLettersrepeat = function guessedLettersrepeat (letter, alreadyguessed) {
-			if (alreadyguessed.length >= 1 && !alreadyguessed.includes(letter)) {
+			if (alreadyguessed.length >= 1 && !alreadyguessed.includes(letter) && !currentWordArr.includes(letter)) {
 				alreadyguessed.push(letter);
 				htmlReplacer("#alreadyguessed", alreadyguessed);
 			}
 		};
 			
-
 	  	var displayWord = function displayWord(currentWordArr, correctlyGuessed) {
-	  		for(let i = 0; i < currentWordArr.length;i++){
-	  			if (letter === currentWordArr[i]){
+	  		for(let i = 0; i < currentWordArr.length;i++) {
+	  			if (letter === currentWordArr[i]) {
 	  				correctlyGuessed.splice(currentWordArr.indexOf(letter, [i]), 1, letter);
 					htmlReplacer("#correctlyGuessed", correctlyGuessed);
 	  			}
 	  		}
 	  	};
 
+	  	// inCorrectWord(letter, currentWordArr, alreadyguessed);
 	  	guessedLetters(letter, alreadyguessed);
 	  	guessedLettersrepeat(letter, alreadyguessed);
 	  	displayWord(currentWordArr, correctlyGuessed); 
 	}
+
